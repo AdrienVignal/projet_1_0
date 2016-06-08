3c664c32947c09a92a3b9dc41e9f0e2105288c5b
deneuxan <antonin27400@gmail.com>
2016-06-08 15:57:54 +0200

    TU PEUX PULL CA =p

--- declarations.h
+++ declarations.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef DECLARATIONS_H
 #define DECLARATIONS_H
 
@@ -34,41 +34,5 @@ class Expression ;
 
 
 #endif // DECLARATIONS_H
-=======
-#ifndef DECLARATIONS_H
-#define DECLARATIONS_H
-
-#include <QString>
-#include <QTextStream>
-#include <QObject>
-#include <QDebug>
-#include <QVector>
-#include <QStack>
-#include <math.h>
-#include <QRegExp>
-#include<string>
-#include<iostream>
-#include<map>
-#include <algorithm>
-#include <fstream>
-#include <deque>
-#include <QSet>
-#include <QMap>
-
-//fichier faisant les inclusions de bibliothèques necessaire, ainsi que la décalaration de quelques class indispensables
-
-struct Attributs ;
-class Literale  ;
-class Controleur ;
-class operateur ;
-class LiteraleManager ;
-class Pile ;
-class Complex ;
-class Program ;
-class Item ;
-class Expression ;
 
 
-
-#endif // DECLARATIONS_H
->>>>>>> c9539195318c65cb60efa2ffe8cbf55b9dd7361d
--- expression.h
+++ expression.h
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-#ifndef EXPRESSION_H
-#define EXPRESSION_H
-#include "declarations.h"
-
-QString convertToPolish(QString s) ;
-
-class Expression : public Literale{
-private:
-    QString exp;
-    Expression(Attributs a):exp(a.s){convertToPolish(exp) ; }
-    Expression(const Program& e);
-    Expression& operator=(const Program& e);
-    friend class LiteraleManager;
-    friend class ExpressionManager;
-    ~Expression() {}
-public:
-    void eval() const;
-    Attributs getValue() const{return Attributs(0,1,0,1,exp) ;}
-    QString toString() const ;
-
-};
-
-
-
-#endif // EXPRESSION_H
-=======
 #ifndef EXPRESSION_H
 #define EXPRESSION_H
 #include "declarations.h"
@@ -51,4 +24,4 @@ QString convertToPolish(QString s) ;
 
 
 #endif // EXPRESSION_H
->>>>>>> c9539195318c65cb60efa2ffe8cbf55b9dd7361d
+
